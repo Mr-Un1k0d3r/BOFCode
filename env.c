32be24a2234eebe746f67135256c137fe46c4cdb
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2023-11-10 14:36:54 -0500

    Update env.c

--- env.c
+++ env.c
@@ -8,20 +8,20 @@ DECLSPEC_IMPORT FARPROC WINAPI kernel32$GetProcAddress(HANDLE, CHAR*);
 DECLSPEC_IMPORT HANDLE WINAPI kernel32$LoadLibraryA(CHAR*);
 
 FARPROC Resolver(CHAR *lib, CHAR *func) {
-    FARPROC ptr = kernel32$GetProcAddress(kernel32$LoadLibraryA(lib), func);
-    return ptr;
+	FARPROC ptr = kernel32$GetProcAddress(kernel32$LoadLibraryA(lib), func);
+	return ptr;
 }
 
 VOID go() {
 	FARPROC GetEnvironmentStrings = Resolver("kernel32.dll", "GetEnvironmentStrings");
 	FARPROC strlen = Resolver("msvcrt.dll", "strlen");
 	FARPROC FreeEnvironmentStrings = Resolver("kernel32.dll", "FreeEnvironmentStringsA");
-    LPCH env = GetEnvironmentStrings();
+	LPCH env = GetEnvironmentStrings();
 	LPCH start = env;
-    while(env[0] != 0x00) {
-        printf("%s\n", env);
-        env += strlen(env) + 1;
-    }
+	while(env[0] != 0x00) {
+		printf("%s\n", env);
+		env += strlen(env) + 1;
+	}
 	
 	FreeEnvironmentStrings(start);
 }
