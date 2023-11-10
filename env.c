fd18c69fabb25ecd39e4f1046e8718d7e477495f
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2023-11-10 14:24:29 -0500

    Add files via upload

---
+++ env.c
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <processenv.h>
+#include <stdio.h>
+#include "beacon.h"
+
+#define printf(format, args...) { BeaconPrintf(CALLBACK_OUTPUT, format, ## args); }
+DECLSPEC_IMPORT FARPROC WINAPI kernel32$GetProcAddress(HANDLE, CHAR*);
+DECLSPEC_IMPORT HANDLE WINAPI kernel32$LoadLibraryA(CHAR*);
+
+FARPROC Resolver(CHAR *lib, CHAR *func) {
+    FARPROC ptr = kernel32$GetProcAddress(kernel32$LoadLibraryA(lib), func);
+    return ptr;
+}
+
+VOID go() {
+	FARPROC GetEnvironmentStrings = Resolver("kernel32.dll", "GetEnvironmentStrings");
+	FARPROC strlen = Resolver("msvcrt.dll", "strlen");
+    LPCH env = GetEnvironmentStrings();
+    while(env[0] != 0x00) {
+        printf("%s\n", env);
+        env += strlen(env) + 1;
+    }
+}
